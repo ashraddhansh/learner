1c7cb133ff6fee8d7a42c2e4df128d1d1183be2b
ashraddhansh <126281805+ashraddhansh@users.noreply.github.com>
2023-12-31 01:11:57 +0530

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -1,6 +1,6 @@
 from googlesearch import search
 
-query = "Best Python libraries for data science"
-results = search(query, tld="com", lang="en", num=5, stop=3, pause=2)
+query = input("what do you want to search?\n")
+results = search(query, lang="en",num_results=15,sleep_interval=5)
 for result in results:
     print(result)
